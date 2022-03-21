a98a59094890f73442c106da45d8d32ff8fdd314
sashi311 <102043062+sashi311@users.noreply.github.com>
2022-03-21 20:09:16 +0530

    Learnif-Condition statement
    Condition Statements

---
+++ LearnIf.java
@@ -0,0 +1,20 @@
+package week1.learndatatyoe;
+
+public class LearnIf {
+	
+ public static void main(String[] args) {
+	 //main is entry door
+	 //condition statement
+	 //problem -even or odd
+	 //-,+,/,*,=,==,
+	 //%-mod operator,4%2>gives remainder 0
+	 //Divide by 2
+	 //if remainder is 0,then it is a even number
+	 //if(true)-it executes
+	 //if(false)-it does not executes
+	int number=26;
+	if (number%2==0) {
+		System.out.println("Even Number");
+	}
+}
+}
