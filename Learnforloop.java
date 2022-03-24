5fc59c4e2dd95fa8ea372fafd00c248d98e1442e
sashi <102043062+sashi311@users.noreply.github.com>
2022-03-24 21:14:23 +0530

    Add files via upload

---
+++ Learnforloop.java
@@ -0,0 +1,17 @@
+package SeleniumBasics;
+
+public class Learnforloop {
+public static void main(String[] args) {
+//	System.out.println(1);
+	//System.out.println(2);
+	//System.out.println(3);
+	//instead of doing this i use for loop
+	//syntax
+	//intialisation condition increment
+	for (int i =1; i<=50; i++) {
+		 //i+++=i+1 ex=i=41+1=42
+			System.out.println(i);
+		
+	}
+}
+}
---
+++ Learnifandforloop.java
@@ -0,0 +1,13 @@
+package SeleniumBasics;
+
+public class Learnifandforloop {
+	public static void main(String[] args) {
+		
+	
+	for (int i = 0; i < 50; i++) {
+		if (i%2==0) {
+		System.out.println("if it is even number"+i);	
+		}
+	}
+}
+}
