e5fb814d516a18df67fc401c13870c3417db522e
sashi <102043062+sashi311@users.noreply.github.com>
2022-03-24 20:57:27 +0530

    Elseif condition

---
+++ LearnElseif.java
@@ -0,0 +1,16 @@
+package SeleniumBasics;
+
+public class LearnElseif {
+public static void main(String[] args) {
+int number=0;
+     if (number==0) {
+	System.out.println("neutral Number");
+}
+     else if (number%2==0) {
+		System.out.println("even number");
+	}	
+     else {
+	System.out.println("odd number");
+	}
+}
+}
