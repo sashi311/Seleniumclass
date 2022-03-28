5c03cd9af94b5d7c55ee8a8334830418cb7e234e
sashi <102043062+sashi311@users.noreply.github.com>
2022-03-28 18:16:57 +0530

    Add files via upload

---
+++ learnbreak.java
@@ -0,0 +1,16 @@
+package SeleniumBasicsday2;
+
+import java.util.Iterator;
+
+public class learnbreak {
+public static void main(String[] args) {
+	//when the value is 5,it should come out of the loop
+	for(int i=0; i<=10; i++) {
+	if(i==6) {
+		break;
+	}
+	System.out.println(i);
+}
+
+}
+}
---
+++ learnswitch.java
@@ -0,0 +1,40 @@
+package SeleniumBasicsday2;
+
+public class learnswitch {
+public static void main(String[] args) {
+	//switch-it matches the int value to the followed value
+	//any datatype, expersion and string can be passed in int value 
+	//break -gives the absoulte output
+	//without giving break the output gets followd after the matching value
+	int number=200;
+	switch (number) {
+	case 100: 
+		System.out.println(100);
+		break;
+	case 200:
+		System.out.println(200);
+		break;
+	case 300:
+		System.out.println(300);
+	break;		
+	default:
+		System.out.println("amount is invalid");;
+	}
+	//Based on experion 
+	int a=2;
+	int b=4;
+	switch (b/2) {
+	case 1: 
+		System.out.println(1);
+		break;
+	case 2:
+		System.out.println(2);
+	case 3:
+		System.out.println(3);
+		
+	default:
+		System.out.println("value is invalid");
+	}
+			
+}
+}
