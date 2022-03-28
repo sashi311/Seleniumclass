6c71cd1ce4004bb94e11f7d8578f10d150dd3cf7
sashi <102043062+sashi311@users.noreply.github.com>
2022-03-28 18:22:46 +0530

    Add files via upload

---
+++ learncontinue.java
@@ -0,0 +1,17 @@
+package SeleniumBasicsday2;
+
+public class learncontinue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//continue statement
+for (int i = 0; i <=10; i++) {
+	if (i==7) {
+		continue;
+			}
+	System.out.println(i);
+}
+System.out.println("Out of the loop");
+	}
+
+}
